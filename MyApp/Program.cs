5e905dad964e391a419e95a1cd6e5865ddf39d90
Yevhen <zhenyaz5013@gmail.com>
2024-10-17 23:45:38 +0300

    Add GreetUser function to Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        GreetUser("User");
+    }
+
+    static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
